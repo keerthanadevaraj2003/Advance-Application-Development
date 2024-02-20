838b62d39a80896d581c0349d871f0bb88d3f43b
keerthanadevaraj2003 <113664921+keerthanadevaraj2003@users.noreply.github.com>
2024-02-20 23:06:27 +0530

    Add files via upload

---
+++ frontend/Day1/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+// import{BrowserRouter as Router,Route,Link,Switch} from 'react-router-dom';
+// import Login from './components/login'
+import Signup from './components/Signup'
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <Signup/> 
+      </div>
+      
+    </>
+  )
+}
+
+export default App
